7e2344cd218a47a4bc3514f871be70878de80c76
Steve Yackey <steveyackey@gmail.com>
2022-02-19 20:26:14 -0500

    removed extra files

--- structs/target/debug/deps/structs-96e9ee2a87b3d820.d
+++
@@ -1,5 +0,0 @@
-/home/syackey/code/rustbook/structs/target/debug/deps/structs-96e9ee2a87b3d820: src/main.rs
-
-/home/syackey/code/rustbook/structs/target/debug/deps/structs-96e9ee2a87b3d820.d: src/main.rs
-
-src/main.rs:
--- structs/target/debug/structs.d
+++
@@ -1,1 +0,0 @@
-/home/syackey/code/rustbook/structs/target/debug/structs: /home/syackey/code/rustbook/structs/src/main.rs
