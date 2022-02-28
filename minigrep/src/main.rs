12ecb4ce0cc72925e599943017503c0318493ab9
Steve Yackey <steveyackey@gmail.com>
2022-02-28 14:28:57 -0500

    starting iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,19 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for '{}'", query);
-    println!("In file {}", filename);  
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
-    
+        process::exit(1);
+    }
 }
