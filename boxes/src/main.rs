868d0777b17ca4075bca42a8f9d69ac418f4bf63
Steve Yackey <steveyackey@gmail.com>
2022-03-01 18:54:49 -0500

    ref cycles

---
+++ boxes/src/main.rs
@@ -0,0 +1,68 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let _list = Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil))))));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("after b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("after c = {}", Rc::strong_count(&a));
+    }
+    println!("after c out of scope = {}", Rc::strong_count(&a));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
