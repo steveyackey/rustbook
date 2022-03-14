30ff44c51d4aaf850e3b41e31cb2216c7ebe6bc5
Steve Yackey <steveyackey@gmail.com>
2022-03-13 20:03:58 -0400

    finished book!

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -8,16 +8,17 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(4).expect("ThreadPool size must be > 0");
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!");
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
